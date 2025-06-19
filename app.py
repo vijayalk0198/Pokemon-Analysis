41987ac0a873834776da6f1ccc9322053e02d552
Vijayalakshmi <vijaya@example.com>
2025-06-20 00:37:16 +0100

    first trial

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 # Display background image
-st.image("images/pokeball.jpg", use_column_width=True)
+#st.image("images/pokeball.jpg", use_column_width=True)
+
+
+
+# Load CSS for full-screen background
+with open("css/app.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Placeholder content
+st.title("Pokémon Study")
